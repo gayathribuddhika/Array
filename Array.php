2b3d941f5ee942c46f83dfe78a801dc282b1e819
[Gayathri Buddhika] <[wg.gayathri@gmail.com]>
2019-03-29 15:29:23 +0530

    Array
    Arrays in php

---
+++ Array.php
@@ -0,0 +1,10 @@
+<?php
+
+$colors = array("red","green","blue","yellow","pink");
+echo "I like ".$colors[0].", ".$colors[1]." and ".$colors[2].".". "<br>";
+
+$food = array("pizza","pasta","hoppers","noodles");
+print_r ($food);
+echo "<br>";
+echo $food[3];
+?> 
